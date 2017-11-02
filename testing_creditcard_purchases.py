bc56974de6ddb01bae43238abb7ac751052bdba8
Anjali Mittu <Anjali.Mittu@capitalone.com>
2017-11-02 16:00:30 -0400

    Created a testing file for credit card purchases

---
+++ testing_creditcard_purchases.py
@@ -0,0 +1,90 @@
+import requests
+import json
+
+print("Creating a new customer for testing")
+#Parameter body
+customer_call = {
+    "first_name": "Jane",
+    "last_name": "Doe",
+    "address": {
+        "street_number": "123",
+        "street_name": "Made Up Street",
+        "city": "SmallTown",
+        "state": "MD",
+        "zip": "22837"
+    }
+}
+#POST request
+r = requests.post('http://api.reimaginebanking.com/customers?key=0f35e6aabd46897e9b0185a67a566d65', json=customer_call, headers={'content-type':'application/json'})
+#Show the result of the POST
+print("Result of the POST: " + str(r.status_code))
+if r.status_code == 201:
+    #Result is good
+    customer_body = r.json()
+    customer_id = customer_body[u'objectCreated'][u'_id']
+    #Show customer's id
+    print(json.dumps(customer_body, indent=4) + '\n')
+else:
+    #Result is not good
+    print("Could not create customer" +'\n')
+
+
+
+print("Creating a new account for the customer")
+#Parameter body
+customer_account_call = {
+    "type": "Credit Card",
+    "nickname": "For buying things",
+    "rewards": 40,
+    "balance": 100
+}
+#POST request
+r = requests.post('http://api.reimaginebanking.com/customers/'+customer_id+'/accounts?key=0f35e6aabd46897e9b0185a67a566d65', json=customer_account_call, headers={'content-type':'application/json'})
+#Show the result of the POST
+print("Result of the POST: " + str(r.status_code))
+if r.status_code == 201:
+    #Result is good
+    account_body = r.json()
+    customer_account_id = account_body[u'objectCreated'][u'_id']
+    print(json.dumps(account_body, indent=4) + '\n')
+else:
+    #Result is not good
+    print("Could not create account"  +'\n')
+
+
+
+print("Making a purchase")
+#Parameter body
+purchase_call = {
+    "merchant_id": "57cf75cea73e494d8675efa4",
+    "medium": "balance",
+    "purchase_date": "2017-11-02",
+    "amount": 10.98,
+    "description": "food"
+}
+#POST request
+r = requests.post('http://api.reimaginebanking.com/accounts/'+customer_account_id+'/purchases?key=0f35e6aabd46897e9b0185a67a566d65', json=purchase_call, headers={'content-type':'application/json'})
+#Show the result of the POST
+print("Result of the POST: " + str(r.status_code))
+if r.status_code == 201:
+    #Result is good
+    purchase_body = r.json()
+    print(json.dumps(purchase_body, indent=4) + '\n')
+else:
+    #Result is not good
+    print("Could not make the purchase"  +'\n')
+
+
+
+print("Checking the credit card account")
+#GET request
+r = requests.get('http://api.reimaginebanking.com/accounts/'+customer_account_id+'?key=0f35e6aabd46897e9b0185a67a566d65', json=purchase_call, headers={'content-type':'application/json'})
+#Show the result of the GET
+print("Result of the GET: " + str(r.status_code))
+if r.status_code == 200:
+    #Result is good
+    account_body = r.json()
+    print(json.dumps(account_body, indent=4) + '\n')
+else:
+    #Result is not good
+    print("Could not get the account"  +'\n')
