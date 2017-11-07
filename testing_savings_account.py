556eff473a39a8f5a13d45fc2a4a505701a9095c
Anjali Mittu <Anjali.Mittu@capitalone.com>
2017-11-07 13:59:48 -0500

    Start testing savings account

--- testing_savings_account.py
+++ testing_savings_account.py
@@ -45,10 +45,10 @@ else:
 print("Creating a new account for the customer")
 #Parameter body
 customer_account_call = {
-    "type": "Credit Card",
-    "nickname": "For buying things",
-    "rewards": 40,
-    "balance": 100
+    "type": "Savings",
+    "nickname": "For Saving",
+    "rewards": 0,
+    "balance": 0
 }
 #POST request
 r = requests.post('http://'+host+'/customers/'+customer_id+'/accounts?key='+api_key, json=customer_account_call, headers={'content-type':'application/json'})
@@ -65,68 +65,28 @@ else:
 
 
 
-print("Making a purchase")
+print("Depositing money to account")
 #Parameter body
-purchase_call = {
-    "merchant_id": "57cf75cea73e494d8675efa4",
+deposit_call = {
     "medium": "balance",
-    "purchase_date": "2017-11-02",
-    "amount": 10.98,
-    "description": "food"
+    "transaction_date": "2017-11-07",
+    "amount": 100
 }
 #POST request
-r = requests.post('http://'+host+'/accounts/'+customer_account_id+'/purchases?key='+api_key, json=purchase_call, headers={'content-type':'application/json'})
+r = requests.post('http://'+host+'/accounts/'+customer_account_id+'/deposits?key='+api_key, json=deposit_call, headers={'content-type':'application/json'})
 #Show the result of the POST
 print("Result of the POST: " + str(r.status_code))
 if r.status_code == 201:
-    #Result is good
-    purchase_body = r.json()
-    print(json.dumps(purchase_body, indent=4) + '\n')
-else:
-    #Result is not good
-    print("Could not make the purchase"  +'\n')
-
-
-
-print("Checking the credit card account")
-#GET request
-r = requests.get('http://'+host+'/accounts/'+customer_account_id+'?key='+api_key, headers={'content-type':'application/json'})
-#Show the result of the GET
-print("Result of the GET: " + str(r.status_code))
-if r.status_code == 200:
     #Result is good
     account_body = r.json()
     print(json.dumps(account_body, indent=4) + '\n')
 else:
     #Result is not good
-    print("Could not get the account"  +'\n')
+    print("Could not deposit money"  +'\n')
 
 
 
-print("Creating a bill")
-#Parameter body
-bill_call = {
-    "status": "completed",
-    "payee": "Spotify",
-    "payment_date": "2017-11-02",
-    "recurring_date": 1,
-    "payment_amount": 15.00
-}
-#POST request
-r = requests.post('http://'+host+'/accounts/'+customer_account_id+'/bills?key='+api_key, json=bill_call, headers={'content-type':'application/json'})
-#Show the result of the POST
-print("Result of the POST: " + str(r.status_code))
-if r.status_code == 201:
-    #Result is good
-    bill_body = r.json()
-    print(json.dumps(bill_body, indent=4) + '\n')
-else:
-    #Result is not good
-    print("Could not make the bill"  +'\n')
-
-
-
-print("Checking the credit card account")
+print("Checking the savings account")
 #GET request
 r = requests.get('http://'+host+'/accounts/'+customer_account_id+'?key='+api_key, headers={'content-type':'application/json'})
 #Show the result of the GET
